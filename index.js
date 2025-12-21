c8ceed33068933cd09e18fec6a8adde5cf904611
nishith <nishith.one@gmail.com>
2025-12-21 19:41:26 +0530

    Feature: Basic Express App with Mongoose Setup Done

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/home", (req, res) => {
+  return res.json({
+    success: true,
+    message: "Fetched Home",
+  });
+});
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+
+    console.log("Connected To Database");
+
+    app.listen(PORT, () => {
+      console.log(`Server Running On PORT ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("Not Able To Connect To Database", error);
+    process.exit(1);
+  }
+};
+
+startServer();
