fc7c0f42bd1d5039903efefc4d1549519c0c8aea
nishith <nishith.one@gmail.com>
2025-12-26 18:37:08 +0530

    Feat: Added Theatre Schema

---
+++ models/theatre.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+/**
+ * Defines the schema of theatre resource to be stored on the DB
+ */
+
+const theatreSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    city: {
+      type: String,
+      required: true,
+    },
+    pincode: {
+      type: Number,
+      required: true,
+    },
+    address: String,
+  },
+  { timestamps: true }
+);
+
+const Theatre = mongoose.model("Theatre", theatreSchema);
+
+module.exports = Theatre;
