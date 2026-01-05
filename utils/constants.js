13f8919b2a5140d2b9fb7e7d8ccdae5dd8d5368e
nishith <nishith.one@gmail.com>
2026-01-05 08:03:41 +0530

    feat: added booking schema with mongoose

--- utils/constants.js
+++ utils/constants.js
@@ -4,7 +4,7 @@
  */
 const USER_STATUS = {
   approved: "APPROVED", // User account has been approved
-  pending: "PENDING",   // User account is pending approval
+  pending: "PENDING", // User account is pending approval
   rejected: "REJECTED", // User account has been rejected
 };
 
@@ -14,11 +14,18 @@ const USER_STATUS = {
  */
 const USER_ROLE = {
   customer: "CUSTOMER", // Regular customer
-  admin: "ADMIN",       // Admin user with full privileges
-  client: "CLIENT",     // Client user with limited privileges
+  admin: "ADMIN", // Admin user with full privileges
+  client: "CLIENT", // Client user with limited privileges
+};
+
+const BOOKING_STATUS = {
+  cancelled: "CANCELLED",
+  successfull: "SUCCESSFULL",
+  processing: "IN_PROCESS",
 };
 
 module.exports = {
   USER_ROLE,
   USER_STATUS,
+  BOOKING_STATUS,
 };
