bfbf9213bfd325c813e5546cccdeb97ecc67eb8a
nishith <nishith.one@gmail.com>
2026-01-06 10:23:46 +0530

    feat: added get apis for booking

--- controllers/booking-controller.js
+++ controllers/booking-controller.js
@@ -48,4 +48,59 @@ const update = async (req, res) => {
   }
 };
 
-module.exports = { create, update };
+const getBookings = async (req, res) => {
+  try {
+    const response = await bookingService.getBookings({ userId: req.user });
+    SuccessResponseBody.data = response;
+    SuccessResponseBody.message = "Successfully fetched the bookings";
+    return res.status(StatusCodes.OK).json(SuccessResponseBody);
+  } catch (error) {
+    ErrorResponseBody.err = error;
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(ErrorResponseBody);
+  }
+};
+
+const getAllBookings = async (req, res) => {
+  try {
+    const response = await bookingService.getAllBookings();
+    SuccessResponseBody.data = response;
+    SuccessResponseBody.message = "Successfully fetched the bookings";
+    return res.status(StatusCodes.OK).json(SuccessResponseBody);
+  } catch (error) {
+    ErrorResponseBody.err = error;
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(ErrorResponseBody);
+  }
+};
+
+const getBookingById = async (req, res) => {
+  try {
+    const response = await bookingService.getBookingById(
+      req.params.id,
+      req.user
+    );
+    SuccessResponseBody.data = response;
+    SuccessResponseBody.message = "Successfully fetched the booking";
+    return res.status(StatusCodes.OK).json(SuccessResponseBody);
+  } catch (error) {
+    if (error.err) {
+      ErrorResponseBody.err = error.err;
+      return res.status(error.code).json(ErrorResponseBody);
+    }
+    ErrorResponseBody.err = error;
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(ErrorResponseBody);
+  }
+};
+
+module.exports = {
+  create,
+  update,
+  getBookings,
+  getAllBookings,
+  getBookingById,
+};
