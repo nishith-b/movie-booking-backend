44ffff8dc006a64ff7df585f956112e11dd373f8
nishith <nishith.one@gmail.com>
2026-01-08 21:13:55 +0530

    config: added env config for production and dev db url

--- utils/db.js
+++ utils/db.js
@@ -3,16 +3,20 @@ const Theatre = require("../models/theatre");
 
 const DBConnect = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL);
-
     // Enable debug logs only in development
     if (process.env.NODE_ENV === "development") {
       mongoose.set("debug", true);
+      await mongoose.connect(process.env.DB_URL);
     }
 
     // Sync indexes safely (dev / test only)
     if (process.env.NODE_ENV !== "production") {
       await Theatre.syncIndexes();
+    }
+
+    // Connect to Production DB
+    if (process.env.NODE_ENV === "production") {
+      await mongoose.connect(process.env.PROD_DB_URL);
     }
 
     console.log("Database connected successfully");
